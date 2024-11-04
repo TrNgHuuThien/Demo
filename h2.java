541d0fe9438c0799f43fd48f78e3ffdc01a8dc5d
TrNgHuuThien <huuthien102004@gmail.com>
2024-11-04 19:15:48 +0700

    demo github

---
+++ h2.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class h2 {
+    public static void main(String[] args) {
+        int test = 5 + 9 * 3 - 10 / 2;
+        System.out.println("Check test: " + test);
+    }
+}
